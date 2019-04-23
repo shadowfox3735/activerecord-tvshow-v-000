6cb1d8308fd4a841097773ffe448ebf45476df3e
shadowfox3735 <normanpan180@gmail.com>
2019-04-23 20:03:10 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,7 +3,7 @@ class CreateShows < ActiveRecord::Migration
     create_table :shows do |t|
       t.string :name
       t.string :network
-      t.integer :day
+      t.string :day
       t.integer :rating
   end
 end
