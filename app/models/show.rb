00e44c7e58f11e320eaec29fc9a1f745fa67c0c0
shadowfox3735 <normanpan180@gmail.com>
2019-04-23 20:19:39 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def Show::most_popular_show
-     Show.find_by(rating: 10)
+     Show.find_by(rating: highest_rating)
   end
   
   def Show::lowest_rating
