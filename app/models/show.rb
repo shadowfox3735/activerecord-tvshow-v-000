03c5fc9ab68bd0ff8a84258b72ad4e6af6001b37
shadowfox3735 <normanpan180@gmail.com>
2019-04-23 20:15:23 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,10 +1,9 @@
 class Show < ActiveRecord::Base
   def Show::highest_rating
     Show.maximum(:rating)
-   
   end
   
   def Show::most_popular_show
-    
+     Show.where("rating = ?", 10)
   end
 end
